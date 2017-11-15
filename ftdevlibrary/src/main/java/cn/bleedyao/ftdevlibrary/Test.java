e56f620b37dc9124837843145890db6d278e1875
yaoluhao <logcat001@gmail.com>
2017-11-15 10:03:54 +0800

    the library is run

---
+++ ftdevlibrary/src/main/java/cn/bleedyao/ftdevlibrary/Test.java
@@ -0,0 +1,17 @@
+package cn.bleedyao.ftdevlibrary;
+
+/**
+ * Created by yaoluhao on 15/11/2017.
+ */
+
+public class Test {
+
+    /**
+      * @auther YaoLuHao
+      * create by 15/11/2017 10:00
+      * 测试函数库是否可以正常使用
+      */
+    public void test(){
+        System.out.println("Hello world");
+    }
+}
